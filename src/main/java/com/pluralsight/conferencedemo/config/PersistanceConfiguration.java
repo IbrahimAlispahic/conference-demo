90a3b31d101385cf1b741afe372c846db3295046
IbrahimAlispahic <ialispahic1@etf.unsa.ba>
2020-01-15 22:07:31 +0100

    Bean commented

--- src/main/java/com/pluralsight/conferencedemo/config/PersistanceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistanceConfiguration.java
@@ -9,11 +9,11 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistanceConfiguration {
 
-    @Bean
+ /*   @Bean
     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url("jdbc:postgresql://localhost:5432/conference_app");
         System.out.println("My custom datasource been has been initialized and set");
         return builder.build();
-    }
+    }*/
 }
